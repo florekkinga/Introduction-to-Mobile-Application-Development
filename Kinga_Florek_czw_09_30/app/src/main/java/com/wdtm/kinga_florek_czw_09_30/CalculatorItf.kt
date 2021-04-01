133b06012aa0ed73727726232b007a17e61a8005
florekkinga <florek.kinga@gmail.com>
2021-04-01 18:36:29 +0200

    remove

--- Kinga_Florek_czw_09_30/app/src/main/java/com/wdtm/kinga_florek_czw_09_30/CalculatorItf.kt
+++
@@ -1,9 +0,0 @@
-package com.wdtm.kinga_florek_czw_09_30
-
-interface CalculatorItf {
-    fun addNumber(num: Int)
-    fun addOperation(op: String)
-    fun evaluateFormula(): Double
-    fun setDecimal(boolean: Boolean)
-    fun clear(displayLabel: String = "")
-}
