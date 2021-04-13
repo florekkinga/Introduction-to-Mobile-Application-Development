8753889313b4001bbd46b1433a91227af6906b62
florekkinga <florek.kinga@gmail.com>
2021-04-13 23:57:35 +0200

    update

--- ExchangeRates/app/src/main/java/com/wdtm/kinga_florek_czw_9_30/MainActivity.kt
+++ ExchangeRates/app/src/main/java/com/wdtm/kinga_florek_czw_9_30/MainActivity.kt
@@ -1,5 +1,25 @@
 package com.wdtm.kinga_florek_czw_9_30
 
+/* Kinga Florek, czwartek godzina 9:30
+* Zrealizowane podpunkty:
+*   1. Layout - tak, obsługa sytuacji awaryjny - nie?
+*   2. Ekran startowy posiada 3 przyciski - Kursy walut/Złoto/Przelicznik
+*   3. Obsługa tabeli A
+*   4. Kursy walut:
+*       4a. tak
+*       4b. tak
+*       4c. skorzystałam z innej biblioteki niż była na zajęciach, więc nie było problemu z flagami
+*       4d. tak
+*       4e. tak
+*   5. Złoto:
+*       5a. tak
+*       5b. tak
+*   6. Przelicznik walut:
+*       Stworzyłam layout
+*       6a.
+*       6b.
+**/
+
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
